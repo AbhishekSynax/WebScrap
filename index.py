93c3442a137e8c844451e80954b40113c3b9900c
AbhishekSynax <synaxbots@gmail.com>
2026-01-31 17:11:26 +0530

    Update index.py

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ from telegram.ext import (
     filters,
 )
 
-BOT_TOKEN = '8507762746:AAGSV_Erszej1eZnj5S3MNemfvcCxETX-3k'  # <=== put your bot token 
+BOT_TOKEN = '8507762746:AAEURLVeXEFujLoXcVTql1HWtIG58GDxKJg'  # <=== put your bot token 
 REQUIRED_CHANNEL = -1002613561003  # your channel ID
 ADMIN_USER_ID = 7998441787  # your Telegram ID
 
